b97c7220c1836ea4788bc60bd1d01082b361d9e5
mayada <maydaamed627@gmail.com>
2025-04-16 22:23:20 +0200

    HomePage edited

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  backgrounds: string[] = [
+    '/homeTwo.png',
+    '/back1.jpg',
+    '/back.jpg'
+  ];
+  currentIndex: number = 0;
+  autoSlideInterval: any;
+  userInteractionTimeout: any;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextBackground();
+    }, 2000); 
+  }
+
+  stopAutoSlideTemporarily() {
+    clearInterval(this.autoSlideInterval);
+    clearTimeout(this.userInteractionTimeout);
+    this.userInteractionTimeout = setTimeout(() => {
+      this.startAutoSlide();
+    }, 2000); 
+  }
+
+  nextBackground() {
+    this.currentIndex = (this.currentIndex + 1) % this.backgrounds.length;
+    this.stopAutoSlideTemporarily();
+  }
+
+  prevBackground() {
+    this.currentIndex = (this.currentIndex - 1 + this.backgrounds.length) % this.backgrounds.length;
+    this.stopAutoSlideTemporarily();
+  }
+
+  open: boolean = false;         // للقائمة الجانبية
+  showLogout: boolean = false;  // لزر تسجيل الخروج
+  toggleMenu() {
+    this.open = !this.open;
+  
+    // لو فتحت القائمة، اقفلي زر تسجيل الخروج
+    if (this.open) {
+      this.showLogout = false;
+    }
+  }
+
+  
+
+
+  logout() {
+    this.authService.logout();
+    console.log('تم تسجيل الخروج');
+    this.router.navigate(['/login']);
+  }
+   // لو فتحت زر تسجيل الخروج، اقفلي القائمة
+   toggleLogoutButton() {
+    this.showLogout = !this.showLogout;
+  
+    // لو فتحت زر تسجيل الخروج، اقفلي القائمة الجانبية
+    if (this.showLogout) {
+      this.open = false;
+    }
+  }
+
+}
