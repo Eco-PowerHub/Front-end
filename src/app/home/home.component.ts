4faa7b68941f1377fad692a7726eb3189523e585
mayada <maydaamed627@gmail.com>
2025-04-17 18:14:43 +0200

    HomePage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ElementRef, ViewChild  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
@@ -31,6 +31,34 @@ export class HomeComponent implements OnInit {
       this.nextBackground();
     }, 2000); 
   }
+  @ViewChild('targetSection') targetSection!: ElementRef;
+
+  scrollToSection() {
+    const element = this.targetSection.nativeElement;
+    const targetPosition = element.getBoundingClientRect().top + window.pageYOffset;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1100; // المدة بالميلي ثانية (زوديها لو عايزة أبطأ)
+    let startTime: number | null = null;
+  
+    const ease = (t: number, b: number, c: number, d: number): number => {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+  
+    const animation = (currentTime: number) => {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+  
+    requestAnimationFrame(animation);
+  }
+  
 
   stopAutoSlideTemporarily() {
     clearInterval(this.autoSlideInterval);
