be01ee207104535d55d5c79757282d9eafc5a5d5
nada <nada.elhawaryy3@gmail.com>
2025-04-17 17:00:59 +0200

    products with fetch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { SuccessfulSignupComponent } from './successful-signup/successful-signup.component';
 import { SendOtpComponent } from './send-otp/send-otp.component';
+import { CartComponent } from '../cart/cart.component';
+import { ProductDisplayComponent } from '../product-display/product-display.component';
 
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/signup', pathMatch: 'full' },
+    { path: '', redirectTo: '/products', pathMatch: 'full' },
     {path: 'signup', component:SignupPageComponent},
     {path: 'signup-success', component:SuccessfulSignupComponent},
     {path: 'sendotp' , component:SendOtpComponent},
+    {path: 'cart', component:CartComponent},
+    {path: 'products', component:ProductDisplayComponent}
 
 ];
 @NgModule({
--- src/product-display/product-display.component.ts
+++ src/product-display/product-display.component.ts
@@ -2,20 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriesCarouselComponent } from "../categories-carousel/categories-carousel.component";
 import { ProductCardComponent } from "../product-card/product-card.component";
 import { IProduct } from '../models/iproduct';
-import { ProductService } from '../product.service';
+import { ProductService } from '../services/product.service';
+import { CartComponent } from "../cart/cart.component";
+import { ActivatedRoute } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-product-display',
-  imports: [CategoriesCarouselComponent, ProductCardComponent],
+  imports: [CategoriesCarouselComponent, ProductCardComponent, HeaderComponent, FooterComponent],
   templateUrl: './product-display.component.html',
   styleUrl: './product-display.component.css'
 })
 export class ProductDisplayComponent implements OnInit {
   products: IProduct[] = [];
+  selectedCategoryName: string = '';
 
-  constructor(private productService: ProductService) {}
 
-  ngOnInit() {
-    this.products = this.productService.getProducts();
+  categoryNamesMap: { [key: string]: string } = {
+    Batteries: 'البطاريات',
+    Inverters: 'المحولات',
+    SolarPanel: 'الخلايا الشمسية',
+    controlSystem: 'نظام مراقبة',
+    Chargers: 'الشواحن',
+    CablesConductors: 'الكابلات والموصلات',
+    all: 'كل المنتجات'
+  };
+  
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const categoryName = params['categoryName'];
+      if (categoryName && categoryName !== 'all') {
+        this.selectedCategoryName = this.categoryNamesMap[categoryName] || categoryName;
+        this.productService.getProductsByCategory(categoryName).subscribe(res => {
+          this.products = res.data;
+        });
+      } else {
+        this.selectedCategoryName = this.categoryNamesMap['all'];
+        this.productService.getAllProducts().subscribe(res => {
+          this.products = res.data;
+        });
+      }
+    });
   }
 }
+  
